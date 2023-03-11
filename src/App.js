34f0f2249ec877622e8324572f286511644e28cb
THREEhundo <samuelbaik87@gmail.com>
2023-03-10 20:24:40 -0800

    Added task & tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
-	const [tasks, useTasks] = useState([
+	const [showAddTask, setShowAddTask] = useState(false)
+	const [tasks, setTasks] = useState([
 		{
 			'text': 'Get 500 from 12399',
 			'day': 'From 8:00 to 18:00 15.07.22',
@@ -35,10 +37,43 @@ const App = () => {
 			'id': 5
 		}
 	])
+
+	// Add Task
+	const addTask = task => {
+		console.log(task)
+		const id = Math.floor(Math.random() * 1000) + 1
+		const newTask = { id, ...task }
+		setTasks([...tasks, newTask])
+	}
+
+	// Delete Task
+	const deleteTask = id => setTasks(tasks.filter(x => x.id !== id))
+
+	// Toggle Reminder
+	const toggleReminder = id => {
+		setTasks(
+			tasks.map(task =>
+				task.id === id ? { ...task, reminder: !task.reminder } : task
+			)
+		)
+	}
+
 	return (
 		<div className='container'>
-			<Header />
-			<Tasks tasks={tasks} />
+			<Header
+				onAdd={() => setShowAddTask(!showAddTask)}
+				showAdd={showAddTask}
+			/>
+			{showAddTask && <AddTask onAdd={addTask} />}
+			{tasks.length > 0 ? (
+				<Tasks
+					tasks={tasks}
+					onDelete={deleteTask}
+					onToggle={toggleReminder}
+				/>
+			) : (
+				'No tasks to display'
+			)}
 		</div>
 	)
 }
