4e91d7cd0b70f024ca16b45475aaa828829c14f4
THREEhundo <samuelbaik87@gmail.com>
2023-03-10 21:33:05 -0800

    tied to server

--- src/App.js
+++ src/App.js
@@ -24,22 +24,55 @@ const App = () => {
 		return data
 	}
 
+	// Fetch Task
+	const fetchTask = async id => {
+		const res = await fetch(`http://localhost:5000/tasks/${id}`)
+		const data = await res.json()
+
+		return data
+	}
+
 	// Add Task
-	const addTask = task => {
-		console.log(task)
-		const id = Math.floor(Math.random() * 1000) + 1
-		const newTask = { id, ...task }
-		setTasks([...tasks, newTask])
+	const addTask = async task => {
+		const res = await fetch(`http://localhost:5000/tasks`, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify(task)
+		})
+
+		const data = await res.json()
+
+		setTasks([...tasks, data])
 	}
 
 	// Delete Task
-	const deleteTask = id => setTasks(tasks.filter(x => x.id !== id))
+	const deleteTask = async id => {
+		await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'DELETE'
+		})
+		setTasks(tasks.filter(task => task.id !== id))
+	}
 
 	// Toggle Reminder
-	const toggleReminder = id => {
+	const toggleReminder = async id => {
+		const taskToToggle = await fetchTask(id)
+		const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify(updTask)
+		})
+
+		const data = await res.json()
+
 		setTasks(
 			tasks.map(task =>
-				task.id === id ? { ...task, reminder: !task.reminder } : task
+				task.id === id ? { ...task, reminder: data.reminder } : task
 			)
 		)
 	}
